77f80c3cd6ba3623bcdfd4e7415f1765daf3f5cc
Florian B <florian_batze@gmx.de>
2018-05-28 14:09:30 +0200

    added 3.6

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -70,5 +70,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/nanovg.dir/DependInfo.cmake"
   "external/glfw-3.2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "source/CMakeFiles/aufgabe_6.dir/DependInfo.cmake"
   "source/CMakeFiles/aufgabe_2_und_3.dir/DependInfo.cmake"
   )
